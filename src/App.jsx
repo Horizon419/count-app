e15d5e8fa6da69e9e13282f6458eefee900d7d31
JMAN <lambejohn6@gmail.com>
2022-07-23 11:03:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+  
+        <h2>COUNT APP</h2>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            +
+          </button><button type="button" onClick={() => setCount((count) => count - 1)}>
+            -
+          </button>
+          <input
+        type="number"
+        placeholder={count}
+      />
+          
+        </p>
+        <button type="button" onClick={() => setCount((count) => count = 0)}>
+            RESET
+          </button>
+      
+      </header>
+    </div>
+  )
+}
+
+export default App
