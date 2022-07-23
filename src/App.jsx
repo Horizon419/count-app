85dcd4d535286f1abe24d726ae34b9c51657fe5b
JMAN <lambejohn6@gmail.com>
2022-07-23 12:44:57 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-  
         <h2>COUNT APP</h2>
         <h1>{count}</h1>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            +
-          </button><button type="button" onClick={() => setCount((count) => count - 1)}>
-            -
-          </button>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>+</button>
+          <button type="button" onClick={() => setCount((count) => count - 1)}>-</button>
         </p>
-        <button type="button" onClick={() => setCount((count) => count = 0)}>
-            RESET
-          </button>
-      
+        <button type="button" onClick={() => setCount((count) => (count = 0))}>
+          RESET
+        </button>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
